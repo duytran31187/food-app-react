ab84cc910a36f0b9d14fe49b63294762fdb76182
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-16 06:34:29 +0700

    [redux] dispatch action

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,19 +1,30 @@
 import React, { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "../MealForm/MealItemForm";
-import CartContext from "../../../store/cart-context";
+// import CartContext from "../../../store/cart-context";
+import { useDispatch } from "react-redux";
+import {counterActions} from "../../../store/";
 
 const MealItem = props => {
     const formattedPRice = `$${props.price.toFixed(2)}`; // first $ is dollar
-    const cartCtx = useContext(CartContext);
+    //const cartCtx = useContext(CartContext);
+    const dispatch = useDispatch();
     const addItemToCardHandler = (amount) => {
-        cartCtx.addItem({
+        dispatch(counterActions.add({
             id: props.id,
             name: props.name,
             amount: amount,
             price: props.price
-        });
+        }));
+        // cartCtx.addItem({
+        //     id: props.id,
+        //     name: props.name,
+        //     amount: amount,
+        //     price: props.price
+        // });
     };
+    
+
     return (
         <li className={classes.meal}>
             <div>
