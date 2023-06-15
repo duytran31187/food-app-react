6b7dd38bad58a84ca2850b7e5f40fe434a8540ad
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-15 08:02:47 +0700

    - useFFect when items changed - use cleanup in reactjs

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCardButton.module.css";
 import CartIcon from "./CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = props => {
+    const [isHighlight, setIsHighlight] = useState(false);
     const cartCtx = useContext(CartContext);
     
     const { items } = cartCtx;
@@ -11,9 +12,25 @@ const HeaderCardButton = props => {
     const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
+
+    const btnClasses =`${classes.button} ${isHighlight ? classes.bump : ''}`;
+    useEffect(() => {
+        if (cartCtx.items.length === 0) {
+            return;
+        }
+        setIsHighlight(true);
+        const timer = setTimeout(() => {
+            setIsHighlight(false);
+        }, 300);
+        return () => { // this is called clean up function in reactjs
+            clearTimeout(timer);
+        };
+
+    }, [cartCtx.items]);
     
+
     return (
-        <button className={classes.button}>
+        <button className={btnClasses}>
             <span className={classes.icon} onClick={props.onShowCart}>
                 <CartIcon />
             </span>
