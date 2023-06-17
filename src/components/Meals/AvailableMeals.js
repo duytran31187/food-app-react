57ebaea587b80bd1fd5a3c6977c4c088ac78e1c2
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-17 11:43:33 +0700

    catch error when fetch and clean up in effect

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,40 +3,45 @@ import classes from './AvailableMeals.module.css';
 import Card from "../Layout/UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+// const DUMMY_MEALS = [
+//     {
+//       id: 'm1',
+//       name: 'Sushi',
+//       description: 'Finest fish and veggies',
+//       price: 22.99,
+//     },
+//     {
+//       id: 'm2',
+//       name: 'Schnitzel',
+//       description: 'A german specialty!',
+//       price: 16.5,
+//     },
+//     {
+//       id: 'm3',
+//       name: 'Barbecue Burger',
+//       description: 'American, raw, meaty',
+//       price: 12.99,
+//     },
+//     {
+//       id: 'm4',
+//       name: 'Green Bowl',
+//       description: 'Healthy...and green...',
+//       price: 18.99,
+//     },
+//   ];
 
 const AvailableMeals = (props) => {
   const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError,  setHttpError] = useState('');
   useEffect(() => {
-    setIsLoading(true);
     const fetchMeals = async () => {
       const response = await fetch('https://food-app-react-97e2c-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
+      
+      if (!response.ok) {
+        throw new Error("something wrong with service");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -50,13 +55,27 @@ const AvailableMeals = (props) => {
         });
       }
       setMeals(loadedMeals);
+      const timer = setTimeout(() => {setIsLoading(false);}, 1000); // setTimeout, just want to see loading effect
+      
+      return () => { // clean up
+        clearTimeout(timer);
+      }
     };
 
-    fetchMeals();
-    const timer = setTimeout(() => {setIsLoading(false);}, 1000); // setTimeout, just want to see loading effect
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
   if (isLoading) {
-    return (<p>IS LOADUNG.....</p>);
+    return (<p>IS LOADING.....</p>);
+  }
+  if (httpError) {
+    return (
+      <section>
+        {httpError}
+      </section>
+    )
   }
     const mealsList = meals.map((meal) => <MealItem
       key={meal.id}
