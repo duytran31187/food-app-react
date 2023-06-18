bad3aaa1786d8b4492e4a144185ba86fe3d2eaf0
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-19 06:40:45 +0700

    Error page to handle error route

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import RootLayout from "./pages/Root";
 import ProductsPage from "./pages/Products";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {path: '/', element: <HomePage />},
       {path: '/products', element: <ProductsPage />}
