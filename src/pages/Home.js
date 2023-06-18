8844040133cce803e8c5891e7334d833394b9198
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-19 06:19:07 +0700

    Link tag to route

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
-
+import { Link } from "react-router-dom";
 
 const HomePage = (props) => {
     return (
         <section>
-            Home page
+            <h1>Home page</h1>
+            <ul>
+                <li>Home</li>
+                <li>
+                    <Link to='/products'> Product pages</Link>
+                </li>
+            </ul>
         </section>
     );
 }
