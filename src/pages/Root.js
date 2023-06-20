b196740ee914dd1108b7e5042a505adc81bd1a3d
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-20 21:06:49 +0700

    useNavigation() to check if route is loading

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/Navigation/MainNavigation";
 
 const RootLayout = (props) => {
+    const navigation = useNavigation();
     return (
         <>
+            {navigation.state === 'loading' && <p>Loading...</p>}
             <MainNavigation />
             <Outlet />
         </>
