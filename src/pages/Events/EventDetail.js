c6c4f2047dbf69d09e6be31295ffb821b21a2dfe
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-21 06:45:03 +0700

    save token after log in successfully and use it for requests

--- src/pages/Events/EventDetail.js
+++ src/pages/Events/EventDetail.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import EventItem from '../../components/Events/EventItem';
 import EventsList from '../../components/Events/EventsList';
+import { getAuthToken } from '../../util/auth';
 
 function EventDetailPage() {
   const { event, events } = useRouteLoaderData('event-detail');
@@ -77,8 +78,13 @@ export async function loader({ request, params }) {
 
 export async function action({ params, request }) {
   const eventId = params.eventId;
+
+  const token = getAuthToken();
   const response = await fetch('http://localhost:8080/events/' + eventId, {
     method: request.method,
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
   });
 
   if (!response.ok) {
