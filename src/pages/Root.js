e5c33452aaeceb79428191259b00ba92392f7bcb
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-21 18:32:38 +0700

    auto logout after 1 hour

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,11 +1,23 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLoaderData, useNavigation, useSubmit } from "react-router-dom";
 import MainNavigation from "../components/Navigation/MainNavigation";
+import { useEffect } from "react";
 
 const RootLayout = (props) => {
-    const navigation = useNavigation();
+    //const navigation = useNavigation();
+    const token = useLoaderData();
+    const submit = useSubmit();
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+        setTimeout(() => {
+            submit(null, {action:'/logout', method: 'post'})
+        }, 1*60*60*1000); // autolog out after 1 hour
+    });
     return (
         <>
-            {navigation.state === 'loading' && <p>Loading...</p>}
+            {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
             <MainNavigation />
             <Outlet />
         </>
